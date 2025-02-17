b7eac7361bf0de154112b2cd3b20118d343bd193
saurabhhh777 <jeeaspirantwallah2022@gmail.com>
2025-02-17 09:27:29 +0530

    nothing new

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-fn main(){
-    let greeting = String::from("hello world");
-    print!("{}",greeting);
+// use std::string;
 
+fn main(){
+    let greeting = String::from("Again OG Gang is here");
+    let first_word  = find_the(&greeting);
+    print!("First word: {}",first_word);
+
+}
+
+fn find_the(s:&String)-> String{
+    let mut ans = String::new();
+    for char in s.chars(){
+        ans.push(char);
+        if char == ' ' {
+            break;
+        }
+        
+    }
+    return  ans;
 }
